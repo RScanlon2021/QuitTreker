1d34ed714eda25dede2955630854d2b2c58b1241
RScanlon2021 <roryscanlon21@protonmail.com>
2021-09-30 08:21:30 +0100

    weekday and month arrays added to app.js

--- app.js
+++ app.js
@@ -162,3 +162,31 @@ const displayBenefits = function (benefits) {
   });
 };
 setInterval(displayBenefits(benefits), 1000);
+
+///////////////////////////////////////////////////////////////////////////////
+
+const calcDaysPassed = (date1, date2) =>
+  Math.floor(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+const weekday = new Array(7);
+weekday[0] = "Sun";
+weekday[1] = "Mon";
+weekday[2] = "Tues";
+weekday[3] = "Wed";
+weekday[4] = "Thurs";
+weekday[5] = "Fri";
+weekday[6] = "Sat";
+
+const month = new Array();
+month[0] = "Jan";
+month[1] = "Feb";
+month[2] = "Mar";
+month[3] = "Apr";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "Aug";
+month[8] = "Sept";
+month[9] = "Oct";
+month[10] = "Nov";
+month[11] = "Dec";
